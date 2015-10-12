493cc367100d1ac0c9dd03a98644aa35ada561a5
Seurat,Alban,NNSA LAUSANNE,eBusiness <Alban.Seurat@nespresso.com>
2015-10-12 16:07:06 +0200

    allow to override default config with data/solr

--- init-solr.sh
+++ init-solr.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sleep 5
 if [ -d /data/solr ]; then
-    bin/solr create_core -c ${SOLR_CORE_NAME:-solrcore} -t /data/solr
+    bin/solr create_core -c ${SOLR_CORE_NAME:-solrcore} -d /data/solr
 else
     bin/solr create_core -c ${SOLR_CORE_NAME:-solrcore}
 fi
