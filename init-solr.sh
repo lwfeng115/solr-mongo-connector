29c1f4863251cd67b225e05bb645acff471e9fb9
Orazio Caccamo <orazio.caccamo@nimbleshift.com>
2015-11-13 08:44:35 +0100

    cluster configuration by zookeeper added

--- init-solr.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sleep 5
-if [ -d /data/solr ]; then
-    bin/solr create_core -c ${SOLR_CORE_NAME:-solrcore} -d /data/solr
-else
-    bin/solr create_core -c ${SOLR_CORE_NAME:-solrcore}
-fi
