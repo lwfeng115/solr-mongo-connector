0379c9cdd3210efd6b48adac5baee718180648de
wenfeng.li <liwenfeng@cwaimai.com>
2016-03-07 11:14:44 +0800

    msg

--- mongo-connector.sh
+++ mongo-connector.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mongo-connector --auto-commit-interval=0 --unique-key=id -m ${MONGODB_HOST:-localhost}:${MONGODB_PORT:-27017} -t http://localhost:8983/solr/${SOLR_CORE_NAME:-solrcore} -d solr_doc_manager --no-dump
+mongo-connector --auto-commit-interval=0 --unique-key=id -m ${MONGODB_HOST:-localhost}:${MONGODB_PORT:-27018} -t http://localhost:8983/solr/${SOLR_CORE_NAME:-solrcore} -d solr_doc_manager --no-dump -n football-news.news
